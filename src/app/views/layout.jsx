d37e3bfb3ce603903bfb2f0cd7d58917d20524c3
Christian Wang <christian.gnaw@gmail.com>
2019-11-26 11:12:02 -0500

    Added bulma and fontawesome support

--- src/app/views/layout.jsx
+++ src/app/views/layout.jsx
@@ -3,10 +3,12 @@ import React from 'react';
 export default ((props) => (
     <html>
         <head>
-            <meta name="viewport" content="width=device-width, inital-scale=1.0"/>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
             <title>{`${(props.title) ? props.title + " | " : ""}CPS530-Term-Project`}</title>
         
-            <link rel="stylesheet" href="/static/styles.css"></link>
+            <link rel="stylesheet" href="/static/styles.css"/>
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css"/>
+    <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"/>
         </head>
         <body>
             {props.children}
