2b0c1f9aca3a53e63e588e8992638c36673b8d53
KingJeremyNg <jeremy97ng@gmail.com>
2019-11-27 16:52:27 -0500

    Trying out infoCard for credit page

--- src/app/views/index.jsx
+++ src/app/views/index.jsx
@@ -2,14 +2,21 @@ import React from 'react';
 
 import Layout from './layout';
 
+import InfoCard from './infoCard';
+
 export default (props) => {
     return (
         <Layout title="Home">
+            <div className="jeremyContainer jeremyFlexHorizontal">
+                <InfoCard bio={["MY MESSAGE", "Adam Aboud", "https://github.com/OneHandKlap", "/static/adam.jpg"]} />
+                <InfoCard bio={["MY MESSAGE", "Christian Wang", "https://github.com/Squishy123", "/static/christian.jpg"]} />
+                <InfoCard bio={["MY MESSAGE", "Jeremy Ng", "https://github.com/KingJeremyNg", "/static/jeremy.jpg"]} />
+            </div>
             <h1>{props.message}</h1>
             <form action="/form">
                 <label>Name</label>
-                <input type="text" name="name"/>
-                <input type="submit" value="submit"/>
+                <input type="text" name="name" />
+                <input type="submit" value="submit" />
             </form>
             <p>{JSON.stringify(props.data)}</p>
         </Layout>)
