e799357fc3e45882336b657e174cc8d762390829
A. Aboud <a.aboud10@gmail.com>
2019-11-29 15:44:54 -0500

    Initial installation page

--- src/app/views/installation.jsx
+++ src/app/views/installation.jsx
@@ -1,20 +1,18 @@
 import React from "react";
-import Layout from ".layout";
-export default props=> {
+import Layout from "./layout";
 
-  render() {
-    return (
-      <Layout title="Installation">
-        <section className="hero is-medium is-info">
-          <div className="hero-body">
-            <h1 className="title is-1">Getting Started</h1>
-            <p className="subtitle is-2">
-              Lets see how to install and implement React, Ronin, and Node.Js
-              <strong>frameworks</strong> used in this project
-            </p>
-          </div>
-        </section>
-      </Layout>
-    );
-  }
+
+export default props=> {
+  return (
+    <Layout title="Installation">
+      <section className="hero is-medium is-info">
+        <div className="hero-body">
+          <h1 className="title is-1">Getting Started</h1>
+          <p className="subtitle is-2">
+            Let's see how to install and implement React, Ronin, and Node.js
+          </p>
+        </div>
+      </section>
+    </Layout>
+  );
 }
