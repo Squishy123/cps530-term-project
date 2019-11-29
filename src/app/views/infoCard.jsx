a620d88ea2e31438f456a8534e23cdb0b4d63ea0
KingJeremyNg <jeremy97ng@gmail.com>
2019-11-29 15:52:46 -0500

    Playing with bulma image css

--- src/app/views/infoCard.jsx
+++ src/app/views/infoCard.jsx
@@ -10,7 +10,7 @@ export default props => {
       <h1>{props.bio[1]}</h1>
       <p>{props.bio[0]}</p>
       <figure class="image is-128x128">
-        <img class="is-rounded" src={props.bio[3]} />
+        <img class="image is-square is-rounded" src={props.bio[3]} />
       </figure>
     </div>
   );
