d934fb02acb5c102d1787eb9ff9139db9b2d4dc4
Christian Wang <christian.gnaw@gmail.com>
2019-11-29 13:08:41 -0500

    Added menu

--- src/app/views/index.jsx
+++ src/app/views/index.jsx
@@ -6,11 +6,5 @@ export default (props) => {
     return (
         <Layout title="Home">
             <h1>{props.message}</h1>
-            <form action="/form">
-                <label>Name</label>
-                <input type="text" name="name"/>
-                <input type="submit" value="submit"/>
-            </form>
-            <p>{JSON.stringify(props.data)}</p>
         </Layout>)
 }
--- src/app/views/layout.jsx
+++ src/app/views/layout.jsx
@@ -1,17 +1,58 @@
 import React from 'react';
 
-export default ((props) => (
-    <html>
-        <head>
-            <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-            <title>{`${(props.title) ? props.title + " | " : ""}CPS530-Term-Project`}</title>
-        
-            <link rel="stylesheet" href="/static/styles.css"/>
-            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css"/>
-    <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"/>
-        </head>
-        <body>
-            {props.children}
-        </body>
-    </html>
-));
+export default class Layout extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            isActive: false //flag for mobile menu expand
+        }
+
+        this.toggleButton = this.toggleButton.bind(this);
+    }
+
+    toggleButton() {
+        console.log("Hello")
+        this.setState({isActive: !this.state.isActive});
+    }
+
+    render() {
+        return (
+            <html>
+                <head>
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                    <title>{`${(this.props.title) ? this.props.title + " | " : ""}CPS530-Term-Project`}</title>
+
+                    <link rel="stylesheet" href="/static/styles.css" />
+                    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css" />
+                    <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js" />
+                </head>
+                <body>
+                    <nav className="navbar is-fixed-top">
+                        <div className="navbar-brand">
+                            <a className="navbar-item">
+                                <h1 className="logo">SYNO</h1>
+                            </a>
+                            <a role="button" className="navbar-burger" onClick={this.toggleButton}>
+                                <span aria-hidden="true"></span>
+                                <span aria-hidden="true"></span>
+                                <span aria-hidden="true"></span>
+                            </a>
+                        </div>
+                        <div className="navbar-menu is-active">
+                            <div className="navbar-start">
+                                <a className="navbar-item">Home</a>
+                            </div>
+                            <div className="navbar-end">
+                                {["Overview", "Installation", "Tutorial", "Demo", "Conclusion", "Credits"].map(p => (
+                                    <a key={p} className="navbar-item" href={`/${p}`}>{p}</a>
+                                ))}
+                            </div>
+                        </div>
+                    </nav>
+                    {this.props.children}
+                </body>
+            </html>
+        )
+    }
+}
+
