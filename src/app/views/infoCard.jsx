affeb6cd745d12b4987e1366dcd44c47add4d390
KingJeremyNg <jeremy97ng@gmail.com>
2019-11-29 17:57:53 -0500

    Smaller portraits, centered portraits and disabled wrapping for links

--- src/app/views/infoCard.jsx
+++ src/app/views/infoCard.jsx
@@ -3,14 +3,14 @@ import React from "react";
 export default props => {
     return (
         <div>
-            <figure className="image is-square">
+            <figure className="image size-300x300 has-image-centered">
                 <img className="image is-rounded" src={props.bio[3]} />
             </figure>
             <section className="section">
                 <div className="box">
                     <h1 className="title is-3">{props.bio[1]}</h1>
                     <p className="subtitle is-4">{props.bio[0]}</p>
-                    <div className="columns">
+                    <div className="columns is-mobile">
                         <div className="column is-one-quarter"></div>
                         <div className="column is-one-quarter">
                             <a href={props.bio[2]} target="_blank">
