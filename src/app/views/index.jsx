e86cf48ea7f58a9c9edb80d52852e93d07030ee6
OneHandKlap <45212899+OneHandKlap@users.noreply.github.com>
2019-12-01 12:11:25 -0500

    homepage edits

--- src/app/views/index.jsx
+++ src/app/views/index.jsx
@@ -13,28 +13,33 @@ export default props => {
             A comprehensive review of web development system frameworks: Node.JS, Ronin, React, and Bulma.
           </p>
           <section className="columns mt-40">
-          <div className="column is-4">
+          <div className="column is-3">
             <div className="content">
-              <img id="logo" src="/static/logos/Node.js-logo.png"></img>
+              <img className="size-480x240" id="logo" src="/static/logos/Node.js-logo.png"></img>
             </div>
           </div>
-          <div className="column is-4">
+          <div className="column is-3">
             <div className="content">
-              <img id="logo" src="/static/logos/ronin-logo.png"></img>
+              <img className="size-480x240"id="logo" src="/static/logos/ronin-logo.png"></img>
             </div>
           </div>
-            <div className="column is-4">
+            <div className="column is-3">
             <div className="content">
-            <img id="logo" src="/static/logos/bulma-logo.png"></img>
+            <img className="size-480x240" id="logo" src="/static/logos/bulma-logo.png"></img>
             </div>
             </div>
-            <div className="column is-4">
+            <div className="column is-3">
             <div className="content">
-            <img id="logo" src="/static/logos/react-logo.png"></img>
+            <img className="size-480x240" id="logo" src="/static/logos/react-logo.png"></img>
             </div>
             </div>
           </section>
         </div>
+      </section>
+      <section className="hero is-dark is-bold">
+        <div className="hero-body">
+          <h3 className="title is-3 has-text-center">Our mission statement: <em>To help new developers learn and use these powerful tools to create something awesome!</em></h3>
+            </div>
       </section>
     </Layout>
   );
