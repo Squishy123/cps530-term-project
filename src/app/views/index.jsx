8d627827790201abb4f11d4350fdb18a982affa4
KingJeremyNg <jeremy97ng@gmail.com>
2019-12-01 13:26:19 -0500

    Merging with master

--- src/app/views/index.jsx
+++ src/app/views/index.jsx
@@ -12,34 +12,28 @@ export default props => {
           <p className="subtitle is-2">
             A comprehensive review of web development system frameworks: Node.JS, Ronin, React, and Bulma.
           </p>
-          <section className="columns mt-40">
-          <div className="column is-3">
-            <div className="content">
-              <img className="size-480x240" id="logo" src="/static/logos/Node.js-logo.png"></img>
-            </div>
+        </div>
+      </section>
+      <section className="hero is-medium is-light is-bold">
+        <div className="hero-body columns mt-40 is-multiline is-vcentered">
+          <div className="column is-3 has-text-centered">
+            <img src="/static/logos/Node.js-logo.png"></img>
           </div>
-          <div className="column is-3">
-            <div className="content">
-              <img className="size-480x240"id="logo" src="/static/logos/ronin-logo.png"></img>
-            </div>
+          <div className="column is-3 has-text-centered">
+            <img src="/static/logos/ronin-logo.png"></img>
           </div>
-            <div className="column is-3">
-            <div className="content">
-            <img className="size-480x240" id="logo" src="/static/logos/bulma-logo.png"></img>
-            </div>
-            </div>
-            <div className="column is-3">
-            <div className="content">
-            <img className="size-480x240" id="logo" src="/static/logos/react-logo.png"></img>
-            </div>
-            </div>
-          </section>
+          <div className="column is-3 has-text-centered">
+            <img src="/static/logos/bulma-logo.png"></img>
+          </div>
+          <div className="column is-3 has-text-centered">
+            <img src="/static/logos/react-logo.png"></img>
+          </div>
         </div>
       </section>
       <section className="hero is-dark is-bold">
         <div className="hero-body">
           <h3 className="title is-3 has-text-center">Our mission statement: <em>To help new developers learn and use these powerful tools to create something awesome!</em></h3>
-            </div>
+        </div>
       </section>
     </Layout>
   );
