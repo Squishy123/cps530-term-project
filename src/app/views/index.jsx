4b3ebace7a50fc91ec7ab3f982c0fc00e291a961
OneHandKlap <45212899+OneHandKlap@users.noreply.github.com>
2019-12-01 11:41:15 -0500

    homepage edits

--- src/app/views/index.jsx
+++ src/app/views/index.jsx
@@ -5,12 +5,37 @@ import Layout from "./layout";
 export default props => {
   return (
     <Layout title="Home">
-      <div className="hero is-info is-bold">
-        <div className="hero-body"></div>
-      </div>
-      <div className="container">
-        <h1 className="title is-5">Demonstration of Ronin + React</h1>
-      </div>
+
+      <section className="hero is-medium is-info is-bold">
+        <div className="hero-body">
+          <h1 className="title is-1">CPS 530 Term Project</h1>
+          <p className="subtitle is-2">
+            A comprehensive review of web development system frameworks: Node.JS, Ronin, React, and Bulma.
+          </p>
+          <section className="columns mt-40">
+          <div className="column is-4">
+            <div className="content">
+              <img id="logo" src="/static/logos/Node.js-logo.png"></img>
+            </div>
+          </div>
+          <div className="column is-4">
+            <div className="content">
+              <img id="logo" src="/static/logos/ronin-logo.png"></img>
+            </div>
+          </div>
+            <div className="column is-4">
+            <div className="content">
+            <img id="logo" src="/static/logos/bulma-logo.png"></img>
+            </div>
+            </div>
+            <div className="column is-4">
+            <div className="content">
+            <img id="logo" src="/static/logos/react-logo.png"></img>
+            </div>
+            </div>
+          </section>
+        </div>
+      </section>
     </Layout>
   );
 };
